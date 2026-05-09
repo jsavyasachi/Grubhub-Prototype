9f028c4fde510cfa3fdc24f7ea06fdaed8fc0047
Savyasachi <jsavyasachi@gmail.com>
2026-05-09 15:24:47 -0700

    refactor(client): convert App component to functional with TDD

--- Grubhub/client/src/App.jsx
+++ Grubhub/client/src/App.jsx
@@ -1,20 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 import Main from "./components/Main";
 import { Provider } from "react-redux";
 import store from "./js/store"
 
 //App Component
-class App extends Component {
-  render() {
-    return (
-      //Use Browser Router to route to different pages
-      <Provider store={store}>
-          <div>
-            <Main />
-          </div>
-      </Provider>
-    );
-  }
-}
+const App = () => {
+  return (
+    //Use Browser Router to route to different pages
+    <Provider store={store}>
+      <div>
+        <Main />
+      </div>
+    </Provider>
+  );
+};
 //Export the App component so that it can be used in index.js
 export default App;
