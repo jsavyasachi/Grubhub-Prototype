a378509ab3ca3626276328031afc3d039c40678d
Savyasachi <jsavyasachi@gmail.com>
2026-05-09 15:23:49 -0700

    refactor(client): convert Main component to functional with TDD

--- Grubhub/client/src/components/Main.jsx
+++ Grubhub/client/src/components/Main.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginUser from "./Login/LoginUser";
 import LoginVendor from "./Login/LoginVendor";
@@ -15,40 +15,35 @@ import SearchResults from "./Search/SearchResults";
 import Restaurant from "./Restaurant/Restaurant";
 import OrderList from "./Order/OrderList";
 
-class Main extends Component {
-  render() {
-    return (
-      <div>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/login-user" element={<LoginUser />} />
-            <Route path="/login-vendor" element={<LoginVendor />} />
-            <Route path="/create-user" element={<CreateUser />} />
-            <Route path="/create-vendor" element={<CreateVendor />} />
+const Main = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login-user" element={<LoginUser />} />
+          <Route path="/login-vendor" element={<LoginVendor />} />
+          <Route path="/create-user" element={<CreateUser />} />
+          <Route path="/create-vendor" element={<CreateVendor />} />
 
-            <Route path="/:id/profile" element={<Profile />} />
-            <Route path="/:id/cart" element={<Cart />} />
-            <Route path="/:id/order" element={<OrderList />} />
-            <Route path="/:id/menu" element={<vendorMenu />} />
-            <Route path="/:id/search" element={<Search />} />
-            <Route path="/results" element={<SearchResults />} />
+          <Route path="/:id/profile" element={<Profile />} />
+          <Route path="/:id/cart" element={<Cart />} />
+          <Route path="/:id/order" element={<OrderList />} />
+          <Route path="/:id/menu" element={<vendorMenu />} />
+          <Route path="/:id/search" element={<Search />} />
+          <Route path="/results" element={<SearchResults />} />
 
-            <Route path="/dish" element={<Dish />} />
-            <Route path="/dish/detail/:dish_id" element={<Dish />} />
-            <Route
-              path="/order/detail/:order_id"
-              element={<OrderDetails />}
-            />
-            <Route
-              path="/restaurant/detail/:restaurant_id"
-              element={<Restaurant />}
-            />
-          </Routes>
-        </BrowserRouter>
-      </div>
-    );
-  }
-}
+          <Route path="/dish" element={<Dish />} />
+          <Route path="/dish/detail/:dish_id" element={<Dish />} />
+          <Route path="/order/detail/:order_id" element={<OrderDetails />} />
+          <Route
+            path="/restaurant/detail/:restaurant_id"
+            element={<Restaurant />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default Main;
